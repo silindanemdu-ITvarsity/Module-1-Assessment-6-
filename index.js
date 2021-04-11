d3df87d5826e7c702ee10c68578e0c6324590046
SilindaM <silindanemdu@gmail.com>
2021-04-11 18:43:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const one=document.getElementById("one");
+
+
+function OneDe() {
+    const head=document.getElementById("heads");
+    head.innerHTML="he";
+    alert("dsds");
+}
+function TwoDe() {
+    var head=document.getElementById("heads");
+    head.innerHTML="two";
+}
